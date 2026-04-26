fc167cd95bd9e5da521c6434241e161f4c2f0e2a
Nandini-Chourasiya <nandinichourasiya75@gmail.com>
2026-04-26 13:02:09 +0530

    initial setup of project management app UI

---
+++ Managment-App/src/App.jsx
@@ -0,0 +1,13 @@
+import NewProject from "./Components/NewProject";
+import ProjectSidebar from "./Components/ProjectSidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar/>
+      <NewProject/>
+    </main>
+  );
+}
+
+export default App;
