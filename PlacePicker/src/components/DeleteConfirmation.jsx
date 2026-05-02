29264fd71216b1f1c3c359e72b051b78fffb54b7
Nandini-Chourasiya <nandinichourasiya75@gmail.com>
2026-05-02 13:46:59 +0530

    UseCallBack Application

--- PlacePicker/src/components/DeleteConfirmation.jsx
+++ PlacePicker/src/components/DeleteConfirmation.jsx
@@ -1,8 +1,29 @@
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
+import { useEffect, useState } from "react";
 
-  setTimeout( () => {
-    onConfirm();
-  },3000)
+const TIMER = 3000;
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+ 
+const [remainingTime , setRemainingTime] = useState(TIMER);
+  
+ useEffect( () => {
+   const interval = setInterval( () => {
+    setRemainingTime( (prevTime) => prevTime-10);
+  } , 10 )
+
+  return () => {
+    clearInterval(interval);
+  };
+ } , []);
+
+  useEffect( () => {
+   const timer = setTimeout( () => {
+      onConfirm();
+    },TIMER)
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  },[onConfirm]);
 
   return (
     <div id="delete-confirmation">
@@ -16,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
