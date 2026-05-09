a0fd27e6fb422cf1d141a8b01b98124d73b0449a
Nandini-Chourasiya <nandinichourasiya75@gmail.com>
2026-05-09 23:50:12 +0530

    Taking user input through ref ,state and used two way binding

---
+++ Forms-and-UserInput/src/components/StateLogin.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues , setEnteredValues] = useState({
+    email : "",
+    password : ""
+  });
+
+  function handleClick(event){
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+  
+  function handleInputChange(Identifier , value){
+    setEnteredValues( (prevValue) => ({
+      ...prevValue,
+      [Identifier] : value
+    }));
+  }
+
+  return (
+    <form onSubmit={handleClick}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" 
+              type="email" 
+              name="email" 
+              onChange={ (event) => handleInputChange( "email" , event.target.value) }
+              value={enteredValues.email}
+            />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input 
+              id="password" 
+              type="password" 
+              name="password" 
+              onChange={ (event) => handleInputChange( "password" , event.target.value) }
+              value={enteredValues.password}
+            />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
